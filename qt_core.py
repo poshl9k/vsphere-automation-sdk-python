f373c91d35338a678617c0d47bb1775f23b69537
poshl9k <poshl9k@gmail.com>
2022-08-04 21:12:19 +0300

    Update qt_core.py

--- qt_core.py
+++ qt_core.py
@@ -1,5 +1,4 @@
 from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
-# import vmware.vapi.settings
-# from vmware.vapi.settings import *
+# from vmware.vapi.settings import 
